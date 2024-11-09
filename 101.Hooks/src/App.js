ac678df08de7bf55494b4f23291055cbb26606ab
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-11-09 16:56:44 +0530

    intro

--- 101.Hooks/src/App.js
+++ 101.Hooks/src/App.js
@@ -1,6 +1,6 @@
-import UseState from './Components/1.UseState/1.UseState';
-import UseEffect from './Components/2.UseEffect/2.UseEffect';
-import UseMemo from './Components/3.UseMemo/3.UseMemo';
+import UseState from './Components/1.UseState/UseState';
+import UseEffect from './Components/2.UseEffect/UseEffect';
+import UseMemo from './Components/3.UseMemo/UseMemo';
 
 function App() {
   return (
