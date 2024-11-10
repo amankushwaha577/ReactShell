48fb1ea5c3336c37b4bf00e25600074ba3c44c63
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-11-10 23:18:38 +0530

    custom hook

---
+++ 104.customhooks/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import useFetch from './CustomHooks/useFetch';
+
+function App() {
+  const { data, loading, error } = useFetch('https://jsonplaceholder.typicode.com/posts');
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {data.map(post => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
