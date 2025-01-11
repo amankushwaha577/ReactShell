3c1a780d5b7f9140553692575b897473bbf4b72e
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-11 12:48:25 +0530

    useMemo()

--- 101.Hooks/src/Components/3.UseMemo/UseMemo.js
+++ 101.Hooks/src/Components/3.UseMemo/UseMemo.js
@@ -24,7 +24,7 @@ export default UseMemo
 
 // Purpose: 
 // ---------------
-// useMemo() hook is used cache returned value from function.
+// useMemo() hook is used cache returned value from function during render so calculation will not happen again again.
 
 
 // const memoizedValue = useMemo(() => calculateFunction(a,b) { return xxx }, [a, b]);
@@ -60,9 +60,22 @@ export default UseMemo
 //    So Child will think its prop is changed And it will re render itself.
 
 
+// Question 1 : So what actually prevent child from re-rendering ?
+// ---------------------------------------------------------------
+// Answer: React.memo (Higher-Order Component)
+// (i).  Purpose: Prevents re-rendering of a component if its props haven't changed.
+// (ii). How it works: It memoizes the entire functional component.
+//       const Component1 = React.memo(({ prop1 }) => {  return ()  });
+//       During First render Component1 will be cached & It will not re-render unless prop1 changes
 
-// Different BW useEffect() vs useMemo():
-//                useEffect                              |                     useMemo
-// 1. handles side effects                               |    1. Memoizes expensive calculations.
-// 2. supports cleanup function
+
+// Question 2 : Do all three (React.memo, useCallback, and useMemo) work during the render phase. ?
+// ---------------------------------------------------------------
+// ✅ Yes, all three (useMemo, useCallback, and React.memo) work during the render phase.
+// A. useMemo → Prevents value recalculation by memoizing the result of a function based on dependencies.
+// B. useCallback → Prevents function recreation unless dependencies change.
+// C. React.memo → Prevents component re-renders if props haven't changed.
+
+
+
 
