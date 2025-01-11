6ddb421ce61f3643c266f4468cddf4dbd5a79f7a
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-12 02:45:20 +0530

    useLayoutEffect

--- 101.Hooks/src/Components/9.UseLayoutEffect/UseLayoutEffect.js
+++ 101.Hooks/src/Components/9.UseLayoutEffect/UseLayoutEffect.js
@@ -14,7 +14,7 @@ function UseLayoutEffect() {
 
   // Op:  Third,  First,  Second
 
-  // Asynchronous: useEffect is asynchronous and runs after the browser has painted and the user has seen the updated UI.
+  // Asynchronous: useEffect is asynchronous and runs after render (after the browser has painted and the user has seen the updated UI).
   // useLayoutEffect is synchronous and runs synchronously after all DOM mutations. 
   // It fires before the browser has painted, making it suitable for operations that need to be done before the user sees the updated UI.
 
@@ -27,3 +27,27 @@ function UseLayoutEffect() {
 }; 
   
 export default UseLayoutEffect;
+
+
+/*
+useLayoutEffect is a React Hook used for running side effects synchronously after the DOM mutations(after rendering) 
+but before the browser paints the screen.
+
+          useLayoutEffect(() => {
+                       Side effect logic here
+            return () => {
+                       Cleanup logic here
+            };
+          }, [dependencies]);
+
+
+Feature	useEffect	vs useLayoutEffect :
+-------------------------------------
+// | 🛠️ **Feature**            | 🎯 **useEffect**                     | 🧩 **useLayoutEffect**            |
+// |----------------------------|--------------------------------------|------------------------------------|
+// | ⏰ **Execution Timing**   | After render & screen update 🖥️      | After render but before paint 🎨  |
+// | 📦 **Use Case**           | Non-blocking side effects ✅         | Blocking layout adjustments 🚧    |
+// | ⚡ **Performance Impact** | Better performance 🚀                | Might slow rendering 🐢           |
+// | 🔄 **Execution Type**     | Asynchronous 🌐                      | Synchronous 🕰️                    |
+
+*/
