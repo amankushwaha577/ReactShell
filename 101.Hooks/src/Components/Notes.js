b76641bfd1c627de214bbb6a76b0e27d31c06458
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-13 00:25:06 +0530

    useState

---
+++ 101.Hooks/src/Components/Notes.js
@@ -0,0 +1,8 @@
+/*
+1. useState() :
+----------------------
+a. Asynchronous : setCount(count + 1); doesn't reflect immediately.
+b. Pure Function : Can't have sideEffects + always returns new state.
+
+
+*/
