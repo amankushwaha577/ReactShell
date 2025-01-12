ec707828e900e271af01a98eb9483538f905ffda
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-13 00:42:03 +0530

    useLayoutEffect

--- 101.Hooks/src/Components/Notes.js
+++ 101.Hooks/src/Components/Notes.js
@@ -5,4 +5,22 @@ a. Asynchronous : setCount(count + 1); doesn't reflect immediately.
 b. Pure Function : Can't have sideEffects + always returns new state.
 
 
+2. useEffect() :
+----------------------
+a. Asynchronous : Doesn't block rendering.
+b. ImPure Function :  Can perform side effects.
+
+
+3. UseMemo() :
+----------------------
+a. Synchronous: ✅ Runs immediately before rendering.
+b. Pure: ✅ No side effects.
+
+
+9. useLayoutEffect() :
+----------------------
+a. Synchronous : It will block UI.
+b. Impure : directly interacts/ manipulate the DOM, which is side effects. + can have other sideEffects like API calling.
+
+
 */
