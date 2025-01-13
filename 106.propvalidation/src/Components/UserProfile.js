532e403579260c68cae36393cf830a7cedd2a114
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-13 21:55:13 +0530

    Required

--- 106.propvalidation/src/Components/UserProfile.js
+++ 106.propvalidation/src/Components/UserProfile.js
@@ -25,36 +25,46 @@ UserProfile.defaultProps = {
 
 export default UserProfile;
 
+/*
+Prop validation ensures components receive the correct types of props, preventing bugs and improving code quality.
 
-// In React, props validation is a mechanism used to ensure that the props passed to a component are of the 
-// correct type and structure. This helps catch bugs and improve the maintainability of your code.
+ComponentName.propTypes = { }
+ComponentName.defaultProps = { }
 
-// 1. Key PropTypes:
-//-----------------------------------------------------------
-// PropTypes.string: validates that the prop is a string.
-// PropTypes.number: validates that the prop is a number.
-// PropTypes.bool: validates that the prop is a boolean.
-// PropTypes.array: validates that the prop is an array.
-// PropTypes.object: validates that the prop is an object.
-// PropTypes.func: validates that the prop is a function.
-// PropTypes.node: validates that the prop can be anything that can be rendered (string, number, element, array, etc.).
-// PropTypes.element: validates that the prop is a React element.
-
-// 2. Optional vs Required Props:
-//-----------------------------------------------------------
-// To specify that a prop is required, use .isRequired, e.g., PropTypes.string.isRequired.
-// If you don't use .isRequired, the prop is optional.
-
-// 3. Default Props:
-//-----------------------------------------------------------
-// To set default values for props, you can use defaultProps like in the example above. 
-// This ensures that the component has sensible default values if props are not provided.
-
-// Note:
-// Ques. If prop is marked as 'isRequired' + also its default value is also set to 'defaultProps'
-//       than=> if it's not passed by parent component ?
-// Ans : isRequired will give warning in console.
-//       even if default value is set.
+1. 📌 Key Prop Types Available:
+------------------------------
+PropTypes.string – String || validates that the prop is a string.
+PropTypes.number – Number
+PropTypes.bool – Boolean
+PropTypes.array – Array
+PropTypes.object – Object
+PropTypes.func – Function
+PropTypes.node – Anything renderable || validates that the prop can be anything that can be rendered (string, number, element, array, etc.).
+PropTypes.element – React element || validates that the prop is a React element.
 
 
-// Note : Support for defaultProps will be removed from function components in a future major release in react.
+2. 📌 Optional vs Required Props:
+-----------------------------------------------------------
+To specify that a prop is required, use .isRequired, e.g., PropTypes.string.isRequired. If you don't use .isRequired, the prop is optional.
+PropTypes.number.isRequired
+
+
+3. 📌 Default Props:
+-----------------------------------------------------------
+To set default values for props, you can use defaultProps like in the example above. 
+This ensures that the component has sensible default values if props are not provided.
+
+
+Note:
+   Ques. If prop is marked as 'isRequired' + also its default value is also set to 'defaultProps'
+         than=> if it's not passed by parent component ?
+   Ans : isRequired will give warning in console.
+         even if default value is set.
+
+
+
+
+Note : Support for defaultProps will be removed from function components in a future major release in react.
+
+
+*/
