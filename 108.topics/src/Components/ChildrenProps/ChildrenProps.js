40081d6de6ffec24a3b590466d070c7b4cef99a8
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-18 16:54:34 +0530

    children props

--- 108.topics/src/Components/ChildrenProps/ChildrenProps.js
+++ 108.topics/src/Components/ChildrenProps/ChildrenProps.js
@@ -11,3 +11,26 @@ const ChildrenProps = () => (
 )
 
 export default ChildrenProps;
+
+/*
+Here’s how it happens, step by step:
+-----------------------------------
+
+1. Parent Component: When you use the Wrapper component, you pass content inside it (e.g., <h1> or <p>).
+                    <Wrapper>
+                        <h1>Hello</h1>
+                    </Wrapper>
+
+2. Wrapper Component: The Wrapper component receives this content as the 'children' prop. It’s like a box that holds whatever is inside the component when it is used.
+                     In the Wrapper component:
+                     const Wrapper = ({ children }) => <div>{children}</div>;
+
+
+3. Rendering: Finally, React will render whatever is inside children. So in this case, it will render:
+                    <div>
+                        <h1>Hello</h1>
+                    </div>
+
+
+
+*/
