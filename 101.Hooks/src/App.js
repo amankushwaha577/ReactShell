8e1d99d8d89522d1925b67ec833d836b4a520029
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-20 23:01:58 +0530

    init

--- 101.Hooks/src/App.js
+++ 101.Hooks/src/App.js
@@ -1,45 +1,47 @@
-import UseState from './Components/1.UseState/UseState';
-import UseEffect from './Components/2.UseEffect/UseEffect';
-import UseMemo from './Components/3.UseMemo/UseMemo';
-import UseCallback from './Components/4.UseCallback/UseCallback';
-import UseReducer from './Components/5.UseReducer/UseReducer';
-import DeaultValue from './Components/6.UseContext/Defaultvalue';
-import MultipleContextAPI from './Components/6.UseContext/MultipleContextAPI';
-import UseContext from './Components/6.UseContext/_1UseContext';
-import DomAccess from './Components/7.UseRef/DomAccess';
-import MutableValueWithNoRerender from './Components/7.UseRef/MutableValueWithNoRerender';
-import MutableValue from './Components/7.UseRef/MutableValueWithNoRerender';
-import ForwardRef from './Components/8.UseImperativehandle/ForwardRef';
-import UseImperativeHandle from './Components/8.UseImperativehandle/UseImperativeHandle';
-import UseLayoutEffect from './Components/9.UseLayoutEffect/UseLayoutEffect';
+
+import UseEffect from './Components/D. SideEffects/6.UseEffect/UseEffect';
+import UseMemo from './Components/C. Caching and Pausing Child Re-render/4.UseMemo/UseMemo';
+import UseCallback from './Components/C. Caching and Pausing Child Re-render/5.UseCallback/UseCallback';
+
+import DeaultValue from './Components/B. Context API/Examples/Defaultvalue';
+import MultipleContextAPI from './Components/B. Context API/Examples/MultipleContextAPI';
+import UseContext from './Components/B. Context API/3.UseContext/_1UseContext';
+import DomAccess from './Components/E. Ref/8.UseRef/DomAccess';
+import MutableValueWithNoRerender from './Components/E. Ref/8.UseRef/MutableValueWithNoRerender';
+import MutableValue from './Components/E. Ref/8.UseRef/MutableValueWithNoRerender';
+import ForwardRef from './Components/E. Ref/9.UseImperativehandle/ForwardRef';
+import UseImperativeHandle from './Components/E. Ref/9.UseImperativehandle/UseImperativeHandle';
+import UseLayoutEffect from './Components/D. SideEffects/7.UseLayoutEffect/UseLayoutEffect';
+
+import UseState from './Components/A. State Management/1.UseState/UseState';
+import UseReducer from './Components/A. State Management/2.UseReducer/UseReducer';
 
 function App() {
   return (
     <div className="App">
+      {/* A. State Management */}
       <UseState/> <hr/> {/* State */}
-
-      <UseEffect/> <hr/> {/* SideEffects */}
-
-      <UseMemo/> <hr/> {/* Value Memoization */}
-      <UseCallback/> <hr/> {/* Function Memoization */}
-
       <UseReducer/> <hr/> {/* State Management : Instead Of changing state again & again Change State Frequently By action */}
 
+      {/* B. Context API */}
       <UseContext />  {/* State Management :  Avoid PropDrilling */}
       <MultipleContextAPI/> 
       <DeaultValue/> <hr/>
 
+      {/* C. Caching and Pausing Child Re-render */}
+      <UseMemo/> <hr/> {/* Value Memoization */}
+      <UseCallback/> <hr/> {/* Function Memoization */}
+
+      {/* D. SIdeEffects */}
+      <UseEffect/> <hr/> {/* SideEffects */}
+      <UseLayoutEffect/> {/* Runs After render but before paint + Synchronously*/}
+
+      {/* E. Refs */}
       <DomAccess/>  {/*  directly referencing and manipulate DOM elements */}
       <MutableValueWithNoRerender/> <hr/> {/* Change Value without causing re-render */}
 
       <ForwardRef/> { /* To Access DOM element of Child,  parent sends a ref and Child take it using forwardRef(props, ref) */}
       <UseImperativeHandle/> <hr/>{/* Child uses useImperativeHandle to restrict parent to completely access DOM */}
-
-      <UseLayoutEffect/> {/* Runs After render but before paint + Synchronously*/}
-      
-
-      
-
     </div>
   );
 }
