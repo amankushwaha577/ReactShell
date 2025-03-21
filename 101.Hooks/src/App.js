7ba0f09fa336600db862708fcb6e2d90fe3f707d
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-22 00:03:35 +0530

    in

--- 101.Hooks/src/App.js
+++ 101.Hooks/src/App.js
@@ -1,18 +1,49 @@
-import { useState, useEffect } from "react";
+
+import UseEffect from './Components/D. SideEffects/6.UseEffect/UseEffect';
+import UseMemo from './Components/C. Caching and Pausing Child Re-render/4.UseMemo/UseMemo';
+import UseCallback from './Components/C. Caching and Pausing Child Re-render/5.UseCallback/UseCallback';
+
+import DeaultValue from './Components/B. Context API/Examples/Defaultvalue';
+import MultipleContextAPI from './Components/B. Context API/Examples/MultipleContextAPI';
+import UseContext from './Components/B. Context API/3.UseContext/_1UseContext';
+import DomAccess from './Components/E. Ref/8.UseRef/DomAccess';
+import MutableValueWithNoRerender from './Components/E. Ref/8.UseRef/MutableValueWithNoRerender';
+import MutableValue from './Components/E. Ref/8.UseRef/MutableValueWithNoRerender';
+import ForwardRef from './Components/E. Ref/9.UseImperativehandle/ForwardRef';
+import UseImperativeHandle from './Components/E. Ref/9.UseImperativehandle/UseImperativeHandle';
+import UseLayoutEffect from './Components/D. SideEffects/7.UseLayoutEffect/UseLayoutEffect';
+
+import UseState from './Components/A. State Management/1.UseState/UseState';
+import UseReducer from './Components/A. State Management/2.UseReducer/UseReducer';
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      {/* A. State Management */}
+      <UseState/> <hr/> {/* State */}
+      <UseReducer/> <hr/> {/* State Management : Instead Of changing state again & again Change State Frequently By action */}
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCount(count + 1);
-      setCount(count + 1);
-      setCount(count + 1);
-      console.log("Updated Count:", count);
-    }, 1000);
-  }, []);
+      {/* B. Context API */}
+      <UseContext />  {/* State Management :  Avoid PropDrilling */}
+      <MultipleContextAPI/> 
+      <DeaultValue/> <hr/>
 
-  return <p>Count: {count}</p>;
+      {/* C. Caching and Pausing Child Re-render */}
+      <UseMemo/> <hr/> {/* Value Memoization */}
+      <UseCallback/> <hr/> {/* Function Memoization */}
+
+      {/* D. SIdeEffects */}
+      <UseEffect/> <hr/> {/* SideEffects */}
+      <UseLayoutEffect/> {/* Runs After render but before paint + Synchronously*/}
+
+      {/* E. Refs */}
+      <DomAccess/>  {/*  directly referencing and manipulate DOM elements */}
+      <MutableValueWithNoRerender/> <hr/> {/* Change Value without causing re-render */}
+
+      <ForwardRef/> { /* To Access DOM element of Child,  parent sends a ref and Child take it using forwardRef(props, ref) */}
+      <UseImperativeHandle/> <hr/>{/* Child uses useImperativeHandle to restrict parent to completely access DOM */}
+    </div>
+  );
 }
 
 export default App;
