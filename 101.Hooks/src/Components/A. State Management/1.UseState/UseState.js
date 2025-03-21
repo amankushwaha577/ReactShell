467a36a3f09d6f8ade27a83fcdbea9b6c339d26a
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-21 22:56:50 +0530

    useState

--- 101.Hooks/src/Components/A. State Management/1.UseState/UseState.js
+++ 101.Hooks/src/Components/A. State Management/1.UseState/UseState.js
@@ -14,7 +14,7 @@ const UseState = () => {
     <>
       <div>{x}</div>
       <button onClick={() => setX((prev) => prev + " Kushwaha")}>Button</button>
-      {/* 3. Functional Updates: Use functional updates (prevState => ...) for changes based on previous state. */}
+      {/* f). Always use the functional form of setState (prevState => newState) if you want changes based on previous state.*/}
     </>
   );
 };
@@ -26,16 +26,35 @@ Definition : Hooks are functions that allows us to work with state and lifecycle
 ----------------------------------------------------------------------------------------
 
 UseState() : This Hook allows us to manage and Hold state.
+
+Points To Remember :
+--------------------
 A. Takes an initial value as an argument and Returns array containing 2 values :
-   [ state , function to change state ].
+   i). State   ii). Function to change state
+
+    const [x, setX]   =   useState("Aman");
+           |   |                      |
+           |    -----> Function        ------------> An Argument
+           |         To Change State
+           |-->State
+
 
 B. State Setter Function is Used to update state.
    a). state can't be update directly. 
-       If we do x ="Aman Kushwaha"  state will change but it will not trigger re-render. So UI will not update.
+       Ex : If we do x ="Aman Kushwaha"  
+            state will change but it will not trigger re-render. So UI will not update.
 
    b). Re-renders: Every state change triggers a re-render of the component.
 
    c). Updating state with the same value doesn’t trigger a re-render.
 
-   d). React batches updates inside event handlers.
+   d). React updates state asynchronously and does not update the state immediately.
+       Updates can be catch through useEffect() | useLayoutEffect() Hook.
+
+   e). React batches state updates.
+       Batching means that multiple state updates inside the same event handler(function) are grouped together, 
+       and React re-renders the component **only once** after all updates are applied.
+
+   f). Always use the functional form of setState (prevState => newState) if you want changes based on previous state.
+       setCount(prevCount => prevCount + 1);
 */
